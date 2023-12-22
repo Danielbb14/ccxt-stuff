f7fb14b088460151753501f83a5058d377bf66cd
Danielbb14 <dbbaldursson@gmail.com>
2023-12-22 18:24:48 +0000

    gitignore

---
+++ oppurtunity.py
@@ -0,0 +1,31 @@
+import csv
+
+def find_highest_profit(csv_file):
+    highest_profit = 0
+    highest_profit_line = ""
+
+    with open(csv_file, newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            potential_profit_str = row.get('Potential Profit (%)')
+            
+            # Check if the value is not None and is a valid float
+            if potential_profit_str is not None and potential_profit_str.replace('.', '', 1).isdigit():
+                potential_profit = float(potential_profit_str)
+                
+                if potential_profit > highest_profit:
+                    highest_profit = potential_profit
+                    highest_profit_line = row
+
+    return highest_profit_line
+
+# Replace 'your_file.csv' with the actual path to your CSV file
+csv_file_path = 'arbitrage_opportunities.csv'
+
+result = find_highest_profit(csv_file_path)
+
+if result:
+    print("Line with the highest potential profit:")
+    print(result)
+else:
+    print("No data found.")
