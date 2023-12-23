6b70b74ca2ad6fcfb8b45e98eddfce53dcf97843
Danielbb14 <dbbaldursson@gmail.com>
2023-12-23 11:01:23 +0000

    a

--- oppurtunity.py
+++ oppurtunity.py
@@ -3,10 +3,12 @@ import csv
 def find_highest_profit(csv_file):
     highest_profit = 0
     highest_profit_line = ""
+    counter = 0
 
     with open(csv_file, newline='') as csvfile:
         reader = csv.DictReader(csvfile)
         for row in reader:
+            counter += 1
             potential_profit_str = row.get('Potential Profit (%)')
             
             # Check if the value is not None and is a valid float
@@ -17,15 +19,16 @@ def find_highest_profit(csv_file):
                     highest_profit = potential_profit
                     highest_profit_line = row
 
-    return highest_profit_line
+    return highest_profit_line,counter
 
 # Replace 'your_file.csv' with the actual path to your CSV file
 csv_file_path = 'arbitrage_opportunities.csv'
 
-result = find_highest_profit(csv_file_path)
+result,counter = find_highest_profit(csv_file_path)
 
 if result:
     print("Line with the highest potential profit:")
     print(result)
+    print(counter)
 else:
     print("No data found.")
